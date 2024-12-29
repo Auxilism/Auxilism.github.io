e717fbe53fd3e9f354a362170534e3013a12399d
Auxilism <darkhalo191@gmail.com>
2024-12-29 20:32:31 +0800

    Fixed da capo inflating the total base ba when base means da capo is lvl 0 so does not exist

--- ab_hexa_op/hexa-da-capo.js
+++ ab_hexa_op/hexa-da-capo.js
@@ -4,9 +4,24 @@ class HexaDaCapo extends HexaMasteryNode
     static #HexaDaCapoLevelScale = 15;
     static #HexaDaCapoBase = 125 - HexaDaCapo.#HexaDaCapoLevelScale;
 
+    _skillLevel1Total;
     constructor(skillInputTotal)
     {
         super(HexaSkillName.DaCapo, skillInputTotal, HexaDaCapo.#HexaDaCapoMaxLevel);
+        this._skillLevel1Total = skillInputTotal;
+    }
+
+    // Since da capo does not exist at lvl 0, the skill base is 0
+    calcSkillBaseTotal(inputStartingLevel)
+    {
+        let skillMultiplier = this.getSkillMultiplierAtLevel(inputStartingLevel);
+        if (skillMultiplier > 0)
+        {
+            this._skillLevel1Total = this._skillLevel1Total / this.getSkillMultiplierAtLevel(inputStartingLevel);
+            console.log(HexaSkillName.DaCapo, this._skillLevel1Total);
+        }
+        this._skillBaseTotal = 0;
+        return this._skillBaseTotal;
     }
 
     #getDaCapoScalingAtLevel(level)
@@ -21,5 +36,10 @@ class HexaDaCapo extends HexaMasteryNode
     getSkillMultiplierAtLevel(level)
     {
         return this.#getDaCapoScalingAtLevel(level) / this.#getDaCapoScalingAtLevel(1);
+    }
+
+    _getScaledUpTotalAtLevel(level)
+    {
+        return this._skillLevel1Total * this.getSkillMultiplierAtLevel(level);
     }
 }
